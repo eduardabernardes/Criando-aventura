2cd8198596acba8c5443f4f2946020cbf97cb8fa
eduardabernardes <163524515+eduardabernardes@users.noreply.github.com>
2024-11-25 07:45:25 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-avanca = document.querySelectorAll('.btn-proximo');
+constavanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
-        constconst atual = document.querySelector('.ativo');
+        const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
